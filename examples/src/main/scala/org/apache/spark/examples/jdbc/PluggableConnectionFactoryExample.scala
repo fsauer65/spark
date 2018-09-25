21cf3680767b0dd5303d609c5b5d66ad94ddf9cd
Frank Sauer <fsauer@kabbage.com>
2018-09-25 16:11:11 -0400

    added example

---
+++ examples/src/main/scala/org/apache/spark/examples/jdbc/PluggableConnectionFactoryExample.scala
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.examples.jdbc
+
+import java.sql.{Connection, Driver, DriverManager}
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.execution.datasources.jdbc.{ConnectionFactoryProvider, DriverRegistry, DriverWrapper, JDBCOptions}
+
+object RDS {
+  def balancedUrl: String = "TBD"
+}
+
+/*
+In real life the above would be something like the below but I did not want to introduce a dependency on AWS-RDS:
+
+object RDS extends Logging {
+  lazy val config: Config = ConfigFactory.load(s"application-${System.getProperty("env")}").getConfig("aggregates")
+
+  lazy val clusterId = config.getString("rds.cluster")
+
+  lazy val rds = AmazonRDSClientBuilder.defaultClient() // requires AWS_REGION environment variable as well as AWS creds
+
+  private var endpoints: Seq[String] = null
+
+  def balancedUrl: String = this.synchronized {
+    // initialize or rotate list of endpoints
+    endpoints = if (endpoints == null) {
+      rds.describeDBInstances().getDBInstances.asScala
+        .filter(i => i.getDBClusterIdentifier == clusterId && i.getDBInstanceStatus == "available")
+        .map(instance => s"${instance.getEndpoint.getAddress}:${instance.getEndpoint.getPort}")
+    } else endpoints.drop(1) ++ endpoints.take(1)
+    endpoints.mkString(s"jdbc:postgresql://",",","/koltpweb")
+  }
+}
+*/
+
+class CustomConnectionFactory extends ConnectionFactoryProvider {
+  import scala.collection.JavaConverters._
+  override def createConnectionFactory(options: JDBCOptions): () => Connection = {
+    val driverClass: String = options.driverClass
+    () => {
+      DriverRegistry.register(driverClass)
+      val driver: Driver = DriverManager.getDrivers.asScala.collectFirst {
+        case d: DriverWrapper if d.wrapped.getClass.getCanonicalName == driverClass => d
+        case d if d.getClass.getCanonicalName == driverClass => d
+      }.getOrElse {
+        throw new IllegalStateException(
+          s"Did not find registered driver with class $driverClass")
+      }
+      // balance reads using RDS api but writes still need to go to the
+      // writer endpoint specified in JDBCOptions
+      val url = if (options.asProperties.getProperty("readOnly") == "true") {
+        RDS.balancedUrl
+      } else options.url
+      driver.connect(url, options.asConnectionProperties)
+    }
+  }
+}
+
+object PluggableConnectionFactoryExample {
+
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    spark.read
+      .format("jdbc")
+      // this will balance queries across RDS read-only replicas
+      .options(Map(
+          JDBCOptions.JDBC_CONNECTION_FACTORY_PROVIDER ->
+          "org.apache.spark.examples.jdbc.CustomConnectionFactory",
+          "readOnly" -> "true" // will enable load balancing on RDS
+      ))
+  }
+}
